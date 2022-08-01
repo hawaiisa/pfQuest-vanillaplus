43718ec5c154a5fca0f64c65da79394a673be125
Hawaiisa <havardsmalas@gmail.com>
2022-08-01 21:51:45 +0200

    Initial commit

---
+++ pfQuest-vanillaplus.lua
@@ -0,0 +1,14 @@
+pfDB["units"]["data"] = {
+    --Yojamba Isle
+    [14875]={["coords"]={[1]={40.1,70.2,33,1800},},["lvl"]="60",["rnk"]="1",}, --Molthor
+    [14902]={["coords"]={[1]={40,72.4,33,1800},},["lvl"]="60",["rnk"]="1",}, --Jin'rokh the Breaker
+    [14903]={["coords"]={[1]={39.8,67.8,33,1800},},["lvl"]="60",["rnk"]="1",}, --Al'tabim the All-Seeing
+    [14904]={["coords"]={[1]={39.8,67.9,33,1800},},["lvl"]="60",["rnk"]="1",}, --Maywiki of Zuldazar
+    [14905]={["coords"]={[1]={41,71.6,33,1800},},["lvl"]="60",["rnk"]="1",}, --Falthir the Sightless
+    [14910]={["coords"]={[1]={40.2,68.9,33,60},},["lvl"]="60",["rnk"]="1",}, --Exzhal
+    [14911]={["coords"]={[1]={40.1,69.2,33,60},},["lvl"]="60",["rnk"]="1",}, --Zandalar Enforcer
+    [14912]={["coords"]={[1]={40.1,69.1,33,60},},["lvl"]="60",["rnk"]="1",}, --Captured Hakkari Zealot
+    [14921]={["coords"]={[1]={40,67.9,33,1800},},["lvl"]="56",["rnk"]="1",}, --Rin'wosho the Trader
+    [15070]={["coords"]={[1]={40.3,69.1,33,1800},},["lvl"]="60",["rnk"]="1",}, --Vinchaxa
+}
+pfDatabase.Reload()
