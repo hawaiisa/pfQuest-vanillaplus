e80171c4622c9472afb82d68a9d9521e00d391f5
Hawaiisa <havardsmalas@gmail.com>
2022-08-01 22:55:12 +0200

    Updated unit coordinates

--- pfQuest-vanillaplus.lua
+++ pfQuest-vanillaplus.lua
@@ -8,7 +8,8 @@ pfDB["units"]["data"] = {
     [14910]={["coords"]={[1]={40.2,68.9,33,60},},["lvl"]="60",["rnk"]="1",}, --Exzhal
     [14911]={["coords"]={[1]={40.1,69.2,33,60},},["lvl"]="60",["rnk"]="1",}, --Zandalar Enforcer
     [14912]={["coords"]={[1]={40.1,69.1,33,60},},["lvl"]="60",["rnk"]="1",}, --Captured Hakkari Zealot
-    [14921]={["coords"]={[1]={40,67.9,33,1800},},["lvl"]="56",["rnk"]="1",}, --Rin'wosho the Trader
+    [14921]={["coords"]={[1]={40,67.9,33,1800},},["lvl"]="63",["rnk"]="1",}, --Rin'wosho the Trader
     [15070]={["coords"]={[1]={40.3,69.1,33,1800},},["lvl"]="60",["rnk"]="1",}, --Vinchaxa
+    [15080]={["coords"]={[1]={41.2,69.4,33,1800},[2]={41.2,69.6,33,1800},},["lvl"]="60",["rnk"]="1",}, --Servant of the Hand
 }
 pfDatabase.Reload()
