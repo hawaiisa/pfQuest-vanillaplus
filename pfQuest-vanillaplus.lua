d0aaffbaa501442110a5b94b6dc61be03c425344
Hawaiisa <havardsmalas@gmail.com>
2022-08-02 19:19:03 +0200

    Fixed addon-breaking misconfiguration
    Made the addon not wipe all unit values but the ones defined.

--- pfQuest-vanillaplus.lua
+++ pfQuest-vanillaplus.lua
@@ -1,17 +1,17 @@
-pfDB["units"]["data"] = {
-    --Zul'Gurub
-    [11383]={["coords"]={[1]={50.7,16.7,33,37800}, [2]={50.7,18.4,33,37800}, [3]={51.4,16.7,33,37800}, [4]={51.4,18.4,33,37800}, },["lvl"]="60",["rnk"]="1",}, --High Priestess Hai'Watna
-    --Yojamba Isle
-    [14875]={["coords"]={[1]={40.1,70.2,33,1800},},["lvl"]="60",["rnk"]="1",}, --Molthor
-    [14902]={["coords"]={[1]={40,72.4,33,1800},},["lvl"]="60",["rnk"]="1",}, --Jin'rokh the Breaker
-    [14903]={["coords"]={[1]={39.8,67.8,33,1800},},["lvl"]="60",["rnk"]="1",}, --Al'tabim the All-Seeing
-    [14904]={["coords"]={[1]={39.8,67.9,33,1800},},["lvl"]="60",["rnk"]="1",}, --Maywiki of Zuldazar
-    [14905]={["coords"]={[1]={41,71.6,33,1800},},["lvl"]="60",["rnk"]="1",}, --Falthir the Sightless
-    [14910]={["coords"]={[1]={40.2,68.9,33,60},},["lvl"]="60",["rnk"]="1",}, --Exzhal
-    [14911]={["coords"]={[1]={40.1,69.2,33,60},},["lvl"]="60",["rnk"]="1",}, --Zandalar Enforcer
-    [14912]={["coords"]={[1]={40.1,69.1,33,60},},["lvl"]="60",["rnk"]="1",}, --Captured Hakkari Zealot
-    [14921]={["coords"]={[1]={40,67.9,33,1800},},["lvl"]="63",["rnk"]="1",}, --Rin'wosho the Trader
-    [15070]={["coords"]={[1]={40.3,69.1,33,1800},},["lvl"]="60",["rnk"]="1",}, --Vinchaxa
-    [15080]={["coords"]={[1]={41.2,69.4,33,1800},[2]={41.2,69.6,33,1800},},["lvl"]="60",["rnk"]="1",}, --Servant of the Hand
-}
+--Units
+--Zul'Gurub
+    pfDB["units"]["data"][11383]={["coords"]={[1]={50.7,16.7,33,37800},[2]={50.7,18.4,33,37800},[3]={51.4,16.7,33,37800},[4]={51.4,18.4,33,37800},[5]={46.8,16.1,33,37800},[6]={48.2,15.5,33,37800}},["lvl"]="60",["rnk"]="1",} --High Priestess Hai'Watna
+--Yojamba Isle
+    pfDB["units"]["data"][14875]={["coords"]={[1]={40.1,70.2,33,1800},},["lvl"]="60",["rnk"] = "1",} --Molthor
+    pfDB["units"]["data"][14902]={["coords"]={[1]={40,72.4,33,1800},},["lvl"]="60",["rnk"]="1",} --Jin'rokh the Breaker
+    pfDB["units"]["data"][14903]={["coords"]={[1]={39.8,67.8,33,1800},},["lvl"]="60",["rnk"]="1",} --Al'tabim the All-Seeing
+    pfDB["units"]["data"][14904]={["coords"]={[1]={39.8,67.9,33,1800},},["lvl"]="60",["rnk"]="1",} --Maywiki of Zuldazar
+    pfDB["units"]["data"][14905]={["coords"]={[1]={41,71.6,33,1800},},["lvl"]="60",["rnk"]="1",} --Falthir the Sightless
+    pfDB["units"]["data"][14910]={["coords"]={[1]={40.2,68.9,33,60},},["lvl"]="60",["rnk"]="1",} --Exzhal
+    pfDB["units"]["data"][14911]={["coords"]={[1]={40.1,69.2,33,60},},["lvl"]="60",["rnk"]="1",} --Zandalar Enforcer
+    pfDB["units"]["data"][14912]={["coords"]={[1]={40.1,69.1,33,60},},["lvl"]="60",["rnk"]="1",} --Captured Hakkari Zealot
+    pfDB["units"]["data"][14921]={["coords"]={[1]={40,67.9,33,1800},},["lvl"]="63",["rnk"]="1",} --Rin'wosho the Trader
+    pfDB["units"]["data"][15070]={["coords"]={[1]={40.3,69.1,33,1800},},["lvl"]="60",["rnk"]="1",} --Vinchaxa
+    pfDB["units"]["data"][15080]={["coords"]={[1]={41.2,69.4,33,1800},[2]={41.2,69.6,33,1800},},["lvl"]="60",["rnk"]="1",} --Servant of the Hand
+
 pfDatabase.Reload()
